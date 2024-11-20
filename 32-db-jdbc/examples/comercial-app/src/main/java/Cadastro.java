6737b6f0af2c9710c828ce28733928844f28e9bb
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-11-20 19:48:36 -0400

    feat: Add example of JDBC transaction management

--- 32-db-jdbc/examples/comercial-app/src/main/java/Cadastro.java
+++ 32-db-jdbc/examples/comercial-app/src/main/java/Cadastro.java
@@ -8,15 +8,6 @@ public class Cadastro {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nome: ");
-        String nome = scanner.nextLine();
-
-        System.out.print("Valor total: ");
-        BigDecimal valorTotal = new BigDecimal(scanner.nextLine());
-
-        System.out.print("Data de pagamento: ");
-        LocalDate dataPagamento = LocalDate.parse(scanner.nextLine(),
-                DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 
         /*
         * String de comando de inserção.
@@ -34,28 +25,55 @@ public class Cadastro {
                 "jdbc:mysql://localhost:3306/comercial", "root", "root123456");
              PreparedStatement comando = conexao.prepareStatement(dml, Statement.RETURN_GENERATED_KEYS)) {
 
-            // Valores dos parâmetros definidos
-            comando.setString(1, nome);
-            comando.setBigDecimal(2, valorTotal);
-            comando.setDate(3, Date.valueOf(dataPagamento));
+            // Desabilita o commit automático da transação
+            // Necessário para controlar a transação (confirmação/reversão)
+            conexao.setAutoCommit(false);
 
-            // Método executeUpdate() executa a instrução e retorna o total de linhas afetadas
-            comando.executeUpdate();
+            try {
+                do {
+                    System.out.print("Nome: ");
+                    String nome = scanner.nextLine();
 
-            /*
-            * Passando Statement.RETURN_GENERATED_KEYS na chamada de prepareStatement,
-            * informamos que queremos as keys geradas, com isso a execução também retornará um
-            * ResultSet com os ids gerados do lado do DB.
-            *
-            * Para recuperar, basta chamar o método getGeneratedKeys(), posicionar o cursor do ResultSet
-            * e ler a primeira coluna.
-            */
-            ResultSet codigoResultSet = comando.getGeneratedKeys();
-            codigoResultSet.next();
-            long codigoGerado = codigoResultSet.getLong(1);
+                    System.out.print("Valor total: ");
+                    BigDecimal valorTotal = new BigDecimal(scanner.nextLine());
 
-            System.out.printf("Venda cadastrada com código %d!%n", codigoGerado);
-        } catch (SQLException e) {
+                    System.out.print("Data de pagamento: ");
+                    LocalDate dataPagamento = LocalDate.parse(scanner.nextLine(),
+                            DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+                    // Valores dos parâmetros definidos
+                    comando.setString(1, nome);
+                    comando.setBigDecimal(2, valorTotal);
+                    comando.setDate(3, Date.valueOf(dataPagamento));
+
+                    // Método executeUpdate() executa a instrução e retorna o total de linhas afetadas
+                    comando.executeUpdate();
+
+                    /*
+                     * Passando Statement.RETURN_GENERATED_KEYS na chamada de prepareStatement,
+                     * informamos que queremos as keys geradas, com isso a execução também retornará um
+                     * ResultSet com os ids gerados do lado do DB.
+                     *
+                     * Para recuperar, basta chamar o método getGeneratedKeys(), posicionar o cursor do ResultSet
+                     * e ler a primeira coluna.
+                     */
+                    ResultSet codigoResultSet = comando.getGeneratedKeys();
+                    codigoResultSet.next();
+                    long codigoGerado = codigoResultSet.getLong(1);
+
+                    System.out.printf("Venda cadastrada com código %d!%n", codigoGerado);
+
+                    System.out.println("Continuar? ");
+                } while ("sim".equalsIgnoreCase(scanner.nextLine()));
+
+                // Confirma a transação caso nenhum erro ocorra
+                conexao.commit();
+            } catch (Exception e) {
+                // Reverte as alterações caso algum erro ocorra
+                conexao.rollback();
+                throw new RuntimeException(e);
+            }
+        } catch (Exception e) {
             System.out.println("Erro cadastrando venda");
             e.printStackTrace();
         }
