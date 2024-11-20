c471f1e3fb5cdfa28d8e842795f449d992033a6a
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-11-20 18:16:25 -0400

    feat: Add example of query execution

--- 32-db-jdbc/examples/comercial-app/src/main/java/Principal.java
+++ 32-db-jdbc/examples/comercial-app/src/main/java/Principal.java
@@ -1,6 +1,5 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.math.BigDecimal;
+import java.sql.*;
 
 public class Principal {
 
@@ -17,11 +16,34 @@ public class Principal {
      * https://dev.mysql.com/downloads/connector/j/
      */
     public static void main(String[] args) {
+        /*
+        * Statement = Interface que representa uma declaração/comando,
+        * para execução de comandos SQL, retornando um ResultSet.
+        *
+        * ResultSet = Interface que representa um conjunto de resultados,
+        * podendo por meio dele percorrer as linhas retornadas e obter os valores das colunas.
+        *
+        * Tanto Connection, quanto Statemet e ResultSet são AutoCloseable,
+        * o que se faz necessário o uso de try-with-resources, porém ResultSet já é fechado
+        * quando o Statement correspondente é fechado.
+        */
         try (Connection conexao = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/comercial", "root", "root123456")) {
-            System.out.println("Conexão aberta!");
+                "jdbc:mysql://localhost:3306/comercial", "root", "root123456");
+             Statement comando = conexao.createStatement();
+             ResultSet resultado = comando.executeQuery("select * from venda")) {
+            while (resultado.next()) {
+                Long id = resultado.getLong("id");
+                String nomeCliente = resultado.getString("nome_cliente");
+                BigDecimal valorTotal = resultado.getBigDecimal("valor_total");
+                Date dataPagamento = resultado.getDate("data_pagamento");
 
-            System.out.println(conexao.getClass());
+                System.out.printf("%d - %s - R$%.2f - %s%n",
+                        id, nomeCliente, valorTotal, dataPagamento);
+            }
+
+//            System.out.println(conexao.getClass());
+//            System.out.println(comando.getClass());
+//            System.out.println(resultado.getClass());
         } catch (SQLException e) {
             System.out.println("Erro de banco de dados");
             e.printStackTrace();
