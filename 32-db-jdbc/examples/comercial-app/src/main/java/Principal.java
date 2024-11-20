59054de9c049fbead60851bcaaeb9e43d9fde03b
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-11-20 17:52:00 -0400

    feat: Add example project of JDBC usage

---
+++ 32-db-jdbc/examples/comercial-app/src/main/java/Principal.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Principal {
+
+    /**
+     * JDBC (Java Database Connectivity)
+     * É a API do Java para acesso a banco de dados, abstraindo as diversas implementações de protocolo de cada DB.
+     * É uma API de mais baixo nível.
+     *
+     * O JDBC já vem junto com o JDK.
+     *
+     * As implementações para a especificação JDBC são os chamados drivers JDBC.
+     *
+     * Neste projeto de exemplo foi utilizado o MySQL ConnectorJ
+     * https://dev.mysql.com/downloads/connector/j/
+     */
+    public static void main(String[] args) {
+        try (Connection conexao = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/comercial", "root", "root123456")) {
+            System.out.println("Conexão aberta!");
+
+            System.out.println(conexao.getClass());
+        } catch (SQLException e) {
+            System.out.println("Erro de banco de dados");
+            e.printStackTrace();
+        }
+    }
+}
